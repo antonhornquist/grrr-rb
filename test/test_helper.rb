600631e2cf492323ca46c5f75c06364fb9468ed9
Anton Hörnquist <anton.hornquist@gmail.com>
2015-03-17 15:03:00 +0100

    fixed whitespace in test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,11 @@ require 'test/unit'
 
 class Test::Unit::TestCase
 	def self.test(name, &block)
-    test_name = "test: #{name} ".to_sym
-    defined = instance_method(test_name) rescue false
-    raise "#{test_name} is already defined in #{self}" if defined
-    define_method(test_name, &block)
-  end
+		test_name = "test: #{name} ".to_sym
+		defined = instance_method(test_name) rescue false
+		raise "#{test_name} is already defined in #{self}" if defined
+		define_method(test_name, &block)
+	end
 end
 
 def save_globals

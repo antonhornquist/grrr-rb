a5b122e4a9d7ba99ac5da71c2258db7d24a3fbd8
Anton Hörnquist <anton.hornquist@gmail.com>
2017-01-09 11:33:44 +0100

    removed test method in test_helper.rb, it's now in ruby core

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,5 @@
 require 'test/unit'
 
-class Test::Unit::TestCase
-	def self.test(name, &block)
-		test_name = "test: #{name} ".to_sym
-		defined = instance_method(test_name) rescue false
-		raise "#{test_name} is already defined in #{self}" if defined
-		define_method(test_name, &block)
-	end
-end
-
 def save_globals
 	$SAVED_GRID_CONTAINER_VIEW_FLASH_ADDED_REMOVED = $GRRR_FLASH_ADDED_REMOVED
 	$SAVED_GRID_VIEW_TRACE_PRESSED = $GRRR_OUTPUT_TRACE_PRESSED

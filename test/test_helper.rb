7fc4f8bf4a99852855b99c505bfb7f53434c1b04
Anton Hörnquist <anton.hornquist@gmail.com>
2017-01-11 13:27:20 +0100

    fixed references to Grrr::Common globals in test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,19 @@
 require 'test/unit'
 
 def save_globals
-	$SAVED_GRID_CONTAINER_VIEW_FLASH_ADDED_REMOVED = $GRRR_FLASH_ADDED_REMOVED
-	$SAVED_GRID_VIEW_TRACE_PRESSED = $GRRR_OUTPUT_TRACE_PRESSED
-	$SAVED_GRID_VIEW_TRACE_LED = $GRRR_OUTPUT_TRACE_LED
+	$SAVED_INDICATE_ADDED_REMOVED_ATTACHED_DETACHED_FLAG = Grrr::Common.indicate_added_removed_attached_detached
+	$SAVED_TRACE_BUTTON_EVENTS_FLAG = Grrr::Common.trace_button_events
+	$SAVED_TRACE_LED_EVENTS_FLAG = Grrr::Common.trace_led_events
 end
 
 def disable_trace_and_flash
-	$GRRR_FLASH_ADDED_REMOVED = false
-	$GRRR_OUTPUT_TRACE_PRESSED = false
-	$GRRR_OUTPUT_TRACE_LED = false
+	Grrr::Common.indicate_added_removed_attached_detached = false
+	Grrr::Common.trace_button_events = false
+	Grrr::Common.trace_led_events = false
 end
 
 def restore_globals
-	$GRRR_FLASH_ADDED_REMOVED = $SAVED_GRID_CONTAINER_VIEW_FLASH_ADDED_REMOVED
-	$GRRR_OUTPUT_TRACE_PRESSED = $SAVED_GRID_VIEW_TRACE_PRESSED
-	$GRRR_OUTPUT_TRACE_LED = $SAVED_GRID_VIEW_TRACE_LED
+	Grrr::Common.indicate_added_removed_attached_detached = $SAVED_INDICATE_ADDED_REMOVED_ATTACHED_DETACHED_FLAG
+	Grrr::Common.trace_button_events = $SAVED_TRACE_BUTTON_EVENTS_FLAG
+	Grrr::Common.trace_led_events = $SAVED_TRACE_LED_EVENTS_FLAG
 end

150bd77567906dc3510b44de271d756849763459
Anton Hörnquist <anton.hornquist@gmail.com>
2017-07-09 23:52:20 +0200

    move switcher tests to container_view_test.rb

--- test/grrr/container_view_test.rb
+++ test/grrr/container_view_test.rb
@@ -675,4 +675,105 @@ class TestContainerView < Test::Unit::TestCase
 			subclass.add_child(View.new_detached(4, 4), Point.new(0, 0))
 		}
 	end
+
+	# view switching
+	test "it should be possible to switch between views by index" do
+		container = ContainerView.new_detached(4, 4)
+		View.new(container, Point.new(0, 0), 4, 4)
+		child2 = View.new_disabled(container, Point.new(0, 0), 4, 4)
+
+		container.switch_to_child_by_index(1)
+
+		assert_equal([child2], container.enabled_children)
+	end
+
+=begin
+	TODO: from switcher tests, review and uncomment suitable tests
+	test "when views are switched the previous current view should be disabled and the new one enabled" do
+		switcher = @switcher
+		child1 = View.new(switcher, Point.new(0, 0), 4, 4)
+		child2 = View.new_disabled(switcher, Point.new(0, 0), 4, 4)
+
+		switcher.value = 1
+
+		assert(child1.is_disabled?)
+		assert(child2.is_enabled?)
+	end
+
+	test "it should not be possible to switch to a view index out of bounds" do
+		switcher = @switcher
+		View.new(switcher, Point.new(0, 0), 4, 4)
+		View.new_disabled(switcher, Point.new(0, 0), 4, 4)
+
+		assert_raise(RuntimeError) { switcher.value = 2 }
+	end
+
+	test "it should not be possible to set a switcher value to nil" do
+		switcher = @switcher
+		View.new(switcher, Point.new(0, 0), 4, 4)
+		View.new_disabled(switcher, Point.new(0, 0), 4, 4)
+
+		assert_raise(RuntimeError) { switcher.value = nil }
+	end
+
+	test "it should be possible to switch between views by view" do
+		switcher = @switcher
+		View.new(switcher, Point.new(0, 0), 4, 4)
+		child2 = View.new_disabled(switcher, Point.new(0, 0), 4, 4)
+
+		switcher.switch_to_view(child2)
+
+		assert_equal(child2, switcher.current_view)
+	end
+
+	test "it should not be possible to switch between views by view to a view that is not a children of switcher" do
+		switcher = @switcher
+		View.new(switcher, Point.new(0, 0), 4, 4)
+		detached_view = View.new_detached(4, 4)
+
+		assert_raise(RuntimeError) { switcher.switch_to_view(detached_view) }
+	end
+
+	test "assuming a switchers children have unique ids it should be possible to switch between views by id" do
+		switcher = @switcher
+		child1 = View.new(switcher, Point.new(0, 0), 4, 4)
+		child1.id = :one
+		child2 = View.new_disabled(switcher, Point.new(0, 0), 4, 4)
+		child2.id = :two
+
+		switcher.switch_to(:two)
+
+		assert_equal(child2, switcher.current_view)
+	end
+
+	test "if a switchers children do not have unique ids it should not be possible to switch between views by id" do
+		switcher = @switcher
+		child1 = View.new(switcher, Point.new(0, 0), 4, 4)
+		child1.id = :one
+		child2 = View.new_disabled(switcher, Point.new(0, 0), 4, 4)
+		child2.id = :one
+
+		assert_raise(RuntimeError) { switcher.switch_to(:one) }
+	end
+
+	test "it should not be possible to switch views by id if no child has the specified id" do
+		switcher = @switcher
+		child1 = View.new(switcher, Point.new(0, 0), 4, 4)
+		child1.id = :one
+		child2 = View.new_disabled(switcher, Point.new(0, 0), 4, 4)
+		child2.id = :two
+
+		assert_raise(RuntimeError) { switcher.switch_to(:three) }
+	end
+
+	test "it should be possible to switch views while a button on switcher view is pressed" do
+		View.new(@switcher, Point.new(0, 0), 4, 4)
+		View.new_disabled(@switcher, Point.new(0, 0), 4, 4)
+		@switcher.press(Point.new(2, 2))
+
+		@switcher.value = 1
+
+		assert_nothing_raised { @switcher.release(Point.new(2, 2)) }
+	end
+=end
 end

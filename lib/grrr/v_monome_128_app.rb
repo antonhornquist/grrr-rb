bc740c2a687734cb1d4d702ca4dd787eb00af008
Anton Hörnquist <anton.hornquist@gmail.com>
2017-07-10 22:44:07 +0200

    fix in v_monome_128_app

--- lib/grrr/v_monome_128_app.rb
+++ lib/grrr/v_monome_128_app.rb
@@ -1,4 +1,4 @@
-class Grrr::VMonome128App < Grrr::AbstractMonome
+class Grrr::VMonome128App < Grrr::MonomeApp
 	def initialize(name, view=nil, origin=nil, create_top_view_if_none_is_supplied=true)
 		super(8, 16, view, origin, create_top_view_if_none_is_supplied)
 	end
